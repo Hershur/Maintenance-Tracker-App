662e742056e1f8f4dbd91bdedf97c6379aa72bf9
Assurance Fagbolagun <assurancefemi@gmail.com>
2019-06-19 15:22:27 +0100

    login page completed..

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let usernameField = document.getElementById('username');
+let usernameLabel = document.getElementById('username-label');
+let passwordField = document.getElementById('password');
+let passwordLabel = document.getElementById('password-label');
+
+usernameField.addEventListener('focus', () => {
+  usernameLabel.style.visibility = 'visible';
+});
+
+usernameField.addEventListener('blur', () => {
+  usernameLabel.style.visibility = 'hidden';
+});
+
+passwordField.addEventListener('focus', () => {
+  passwordLabel.style.visibility = 'visible';
+});
+
+passwordField.addEventListener('blur', () => {
+  passwordLabel.style.visibility = 'hidden';
+});
