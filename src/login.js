462cb64cfd3d7913575a4ff0e54e02963e5834c8
Assurance Fagbolagun <assurancefemi@gmail.com>
2019-07-20 12:08:49 +0100

    improved ui design

--- src/login.js
+++ src/login.js
@@ -6,12 +6,13 @@ login.addEventListener('click', event => {
 
   const userName = document.getElementById('username').value;
   const userPassword = document.getElementById('password').value;
+  const passError = document.getElementById('pass-error');
 
   if (userName == 'user' && userPassword == 'user') {
     window.location.href = 'dashboard.html';
   } else if (userName == 'admin' && userPassword == 'admin') {
     window.location.href = 'dashboard.html';
   } else {
-    alert('You have entered a wrong password');
+    passError.innerHTML = 'You have entered a wrong username or password . . .';
   }
 });
--- src/main.js
+++ src/main.js
@@ -6,6 +6,36 @@ let phoneField = document.getElementById('phone-number');
 let phoneLabel = document.getElementById('phone-label');
 let emailField = document.getElementById('email');
 let emailLabel = document.getElementById('email-label');
+let nameField = document.getElementById('name');
+let nameLabel = document.getElementById('name-label');
+let requestField = document.getElementById('request');
+let requestLabel = document.getElementById('request-label');
+let deptField = document.getElementById('department');
+let deptLabel = document.getElementById('department-label');
+
+requestField.addEventListener('focus', () => {
+  requestLabel.style.visibility = 'visible';
+});
+
+requestField.addEventListener('blur', () => {
+  requestLabel.style.visibility = 'hidden';
+});
+
+nameField.addEventListener('focus', () => {
+  nameLabel.style.visibility = 'visible';
+});
+
+nameField.addEventListener('blur', () => {
+  nameLabel.style.visibility = 'hidden';
+});
+
+deptField.addEventListener('focus', () => {
+  deptLabel.style.visibility = 'visible';
+});
+
+deptField.addEventListener('blur', () => {
+  deptLabel.style.visibility = 'hidden';
+});
 
 phoneField.addEventListener('focus', () => {
   phoneLabel.style.visibility = 'visible';
@@ -23,7 +53,6 @@ emailField.addEventListener('blur', () => {
   emailLabel.style.visibility = 'hidden';
 });
 
-
 usernameField.addEventListener('focus', () => {
   usernameLabel.style.visibility = 'visible';
 });
@@ -39,6 +68,3 @@ passwordField.addEventListener('focus', () => {
 passwordField.addEventListener('blur', () => {
   passwordLabel.style.visibility = 'hidden';
 });
-
-
-
